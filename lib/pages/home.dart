3e127b773fb0f8cb08a9b1e0d3f2909819625adc
adimer <adimer101@gmail.com>
2024-10-28 22:27:26 -0400

    FUncinadno la palciacion de geolocalizacion

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +13,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  MapController _mapController = MapController();
+  var lat = -17.969667;
+  var lon = -67.114658;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    permission();
+    miUbicacion();
+  }
+  permission() async {
+    var status = await Permission.location.status;
+    if (!status.isGranted) {
+      await Permission.location.request();
+    }
+  }
+  miUbicacion() async {
+    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      lat = position.latitude;
+      lon = position.longitude;
+    });
+    _mapController.move(LatLng(lat, lon), 17);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +46,9 @@ class _HomeState extends State<Home> {
         title: Text('Home'),
       ),
       body: FlutterMap(
+        mapController: _mapController,
         options: MapOptions(
-          initialCenter: LatLng(-17.969667, -67.114658), // Center the map over London
+          initialCenter: LatLng(lat, lon),
           initialZoom: 13,
         ),
         children: [
@@ -26,6 +56,16 @@ class _HomeState extends State<Home> {
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
             userAgentPackageName: 'com.example.app',
             // And many more recommended properties!
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(lat, lon),
+                width: 25,
+                height: 25,
+                child: Icon(Icons.location_on, color: Colors.red),
+              ),
+            ],
           ),
           RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
             attributions: [
